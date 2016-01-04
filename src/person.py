99865e25df836c947adf84f0c5f742f80535f179
David Kroell <dkroell@acm.org>
2016-01-04 14:47:28 -0500

    added a person class to handle storage and attributes for each object reference

---
+++ src/person.py
@@ -0,0 +1,43 @@
+# Class that is the defining class for a Contact within the realm of a person's social network.
+# @author davidkroell, ramzirizk
+#
+# This is the class which is instantiated into several references.
+
+class Person:
+    
+    firstName = ""
+    lastName  = ""
+    DOB       = ""
+    
+    def __init__(self, v1, v2, v3):
+        self.firstName = v1
+        self.lastName  = v2
+        self.DOB       = v3
+
+
+    #Accessor Methods for the Person Class.
+        
+    def getFirstName():
+        return self.firstName
+
+    def getLastName():
+        return self.lastName
+    
+    def getDOB():
+        return self.DOB
+
+    #Modifyer methods for the class
+
+    def setFirstName(self, v):
+        self.firstName = v
+
+    def setLastName(self, v):
+        self.lastName = v
+
+    def setDOB(self, v):
+        self.DOB = v
+
+
+    def toString(self):
+        var = self.firstName + ' - ' + self.lastName + ' - ' + self.DOB
+        return var
